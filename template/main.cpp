d94dc0df274b319c4ceabd09bdbac1d4ce9113ef
vovi-z <81784608+vovi-z@users.noreply.github.com>
2022-12-03 11:42:30 -0600

    Added code to template folder

---
+++ template/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <iomanip>
+#include <fstream>
+#include <vector>
+#include <string>
+#include <algorithm>
+
+
+int main() {
+    std::ifstream input("input.txt");
+    
+    return 0;
+}
