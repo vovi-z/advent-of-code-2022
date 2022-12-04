efeb15219ea36b2a4d745662df2d3b05b1dd344c
vovi-z <81784608+vovi-z@users.noreply.github.com>
2022-12-04 08:31:36 -0600

    Added input code to template

--- template/main.cpp
+++ template/main.cpp
@@ -4,10 +4,15 @@
 #include <vector>
 #include <string>
 #include <algorithm>
-
+#include <set>
 
 int main() {
     std::ifstream input("input.txt");
+    std::string temp;
+
+    while(std::getline(input, temp)) {
+
+    }
     
     return 0;
 }
